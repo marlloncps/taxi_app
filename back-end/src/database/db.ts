7bd051e5c6276d1cd19fb3e1f6448ee63e202fb6
Márllon César <87676955+marlloncps@users.noreply.github.com>
2024-11-24 00:04:58 -0300

    fix: trocando mysql2 para mysql2/promise e fazendo um pool ed conexões

--- back-end/src/database/db.ts
+++ back-end/src/database/db.ts
@@ -1,21 +1,16 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || "mysql",
-  user: process.env.MYSQL_user || "root",
+  user: process.env.MYSQL_USER || "root",
   password: process.env.MYSQL_PASSWORD || "root",
   database: process.env.MYSQL_DATABASE || "taxi_app",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err);
-    process.exit(1);
-  }
-  console.log("Conectado ao banco de dados MySQL!");
-});
-
-export default connection;
+export default pool;
