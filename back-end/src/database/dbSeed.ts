f224b956d3a5d0d228a1c553dcafb54e9f802ddd
Márllon César <87676955+marlloncps@users.noreply.github.com>
2024-11-25 17:24:59 -0300

    feat: adicionando created_at timestamp na tabela de rides

--- back-end/src/database/dbSeed.ts
+++ back-end/src/database/dbSeed.ts
@@ -47,6 +47,7 @@ const executeSqlCommands = async (connection: mysql.Pool): Promise<void> => {
     duration TEXT NOT NULL,
     driver JSON NOT NULL,
     value DOUBLE NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`,
   ];
