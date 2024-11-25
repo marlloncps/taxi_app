d292526ee0401f733e068a56373c57897aecfc8a
Márllon César <87676955+marlloncps@users.noreply.github.com>
2024-11-25 02:19:23 -0300

    feat: adicionando rota ride/confirm

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import RideEstimateRouter from "../modules/ride-estimate/ride-estimate.router";
+import RideConfirmRouter from "../modules/ride-confirm/ride-confirm.router";
 
 const router = Router();
 
 router.use("/ride/estimate", RideEstimateRouter);
+router.use("/ride/confirm", RideConfirmRouter);
 
 export default router;
